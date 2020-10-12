188020fea340c95ba9de799cbf64fa61528aa0be
Jéssica Ramírez <jessicaramsa@gmail.com>
2020-10-12 13:56:24 -0500

    archivos

---
+++ archivos.py
@@ -0,0 +1,8 @@
+# realizar un programa que utilice archivos que permita
+# crear usuarios: nombre de usuario (único), contraseña (única), nombre completo, rol (administrativo, operativo, usuario)
+# borrar usuarios
+# agregar usuarios
+# editar
+# buscar un usuario regresar su contraseña (NIP de 4 dígitos y hash)
+# buscar una contraseña y retornar el usuario
+# buscar una contraseña y retornar el usuario
